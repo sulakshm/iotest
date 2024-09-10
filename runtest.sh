99548ae66795d797a41433c4bf071d2cc274276f
lsundararajan <lsundararajan@purestorage.com>
2024-09-10 11:44:17 +0000

    add comments
    Signed-off-by: lsundararajan <lsundararajan@purestorage.com>

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+## setup.sh creates 16 data colors with random data (4K)
+## create the targets(mytargets file under /var/cores/lns) list of devices (tag, path, data color[0-15])
+## run from /var/cores/lns - setup dir
 for p in $(cat mytargets |  awk '{print $2}'); do blkdiscard $p; done
 ./iotest -targets ./mytargets -shuffle
 nohup ./iotest -targets ./mytargets -random > nohup.localtest-$(date +%s).out &
-
--- verify.sh
+++ verify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+## run from setup dir - /var/cores/lns
+## verify based on current targets config
 ./iotest -targets ./mytargets -verify
 
